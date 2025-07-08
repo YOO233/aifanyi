7ca757918ae4ffc2ef21c504053c2219e684e2af
YOO233 <37630021+YOO233@users.noreply.github.com>
2025-07-08 10:51:29 +0800

    Update dify_config.py

--- dify_config.py
+++ dify_config.py
@@ -1,5 +1,5 @@
-API_URL = "http://192.168.0.250:50080/v1/workflows/run"
-API_KEY = "app-GV19S0Xp6rBG3U1hktnNan5M"
+API_URL = ""
+API_KEY = ""
 LOG_CONFIG = {
     "max_bytes": 10 * 1024 * 1024,  # 10MB
     "backup_count": 5,
